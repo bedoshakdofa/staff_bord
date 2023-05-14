c11def3aef310a32ecac58bf209a0b3260613351
abdullah shakdoufa <112889796+bedoshakdofa@users.noreply.github.com>
2023-05-14 18:34:09 +0300

    Update settings.py

--- staff/settings.py
+++ staff/settings.py
@@ -122,7 +122,6 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'staffapp/static'),
-    os.path.join(BASE_DIR,'chatbot/static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
